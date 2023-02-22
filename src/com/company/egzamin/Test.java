8aa3ec9fba11695011e9858a1de82d84a12aa011
Piotr Karol <teropod2601@gmail.com>
2023-02-22 01:01:27 +0100

    egzamin - trzecia próba (coś niecoś potrafię)

--- src/com/company/egzamin/Test.java
+++ src/com/company/egzamin/Test.java
@@ -1,45 +1,85 @@
 package com.company.egzamin;
-
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Test {
-
-    private final double[]g = new double[100];
     public Test() {
-        algortym();
+        start();
     }
 
-    private double funkcja(double[] x){
-        /*
-        double result = 0;
-        for (int i = 0; i < x.length; i++) {
-            result += (i+1)*Math.pow(x[i], i-1);
-        }
-        return result;
-         */
-        return IntStream.range(0, x.length).mapToDouble(i -> (i + 1) * Math.pow(x[i], i - 1)).sum();
-    }
+    private final double[]x = new double[4];
+    private double[]g = new double[x.length];
 
-    private double pochodna(double[] x){
-        return IntStream.range(0, x.length).mapToDouble(i -> ((i + 1)*(i - 1)) * Math.pow(x[i], (i - 1)-1)).sum();
-    }
-
-//    private double[] gradient(double[] x, double[] g) {
-//
-//        return g;
-//    }
-
-    private void algortym(){
-        double[]x = new double[100];
-        double k = 1;
+    private void start(){
         Arrays.fill(x,1);
-        System.out.println("wartość funkcji wynosi: " + funkcja(x));
-        Arrays.setAll(g, i -> ((i + 1) * (i - 1)) * Math.pow(x[i], (i - 1) - 1));
-//        Arrays.stream(g).forEach(System.out::println);
-        double[] temp = new double[100];
-        Arrays.setAll(temp, i-> x[i]+(k*g[i]));
+        g = fPrim(x);
+        IntStream.range(0, g.length).forEach(i -> g[i] *= -1);
+        System.out.println("wartość funkcji wnosi: " + Arrays.stream(f(x)).sum());
+//        System.out.print("\npunkty: \n");
+//        Arrays.stream(x).forEach(System.out::println);
+        System.out.println("\ngradient: ");
+        Arrays.stream(g).forEach(System.out::println);
+        System.out.println("\nwartość pochodnej wynosi: "+Arrays.stream(g).sum());
+        steffen(x);
+    }
+
+    private double[] f(double[] x) {
+        double[] sum = new double[x.length];
+        IntStream.rangeClosed(1, x.length).forEach(i -> sum[i - 1] = (i + 1) * Math.pow(x[i - 1], i - 1));
+        return sum;
+    }
+
+    private double[] fPrim(double[] x) {
+        double[] sum = new double[x.length];
+        IntStream.rangeClosed(1, x.length).forEach(i -> sum[i - 1] = ((i + 1) * (i - 1) * Math.pow(x[i - 1], (i - 1) - 1)));
+        return sum;
+    }
+
+    private void steffen(double[] x) {
+        double krok = 1;
+        double[] temp = new double[x.length];
+        double[] temp2 = new double[x.length];
+        IntStream.range(0, temp.length).forEach(i -> temp[i] = x[i] + (krok * g[i]));
+        System.out.println("\nnowe punkty bez optymalizacji kroku: ");
         Arrays.stream(temp).forEach(System.out::println);
+        System.out.println("\nwartość funkcji z krokiem bez optymalizacji: " + Arrays.stream(f(temp)).sum());
+        System.out.println();
+        Arrays.stream(fPrim(temp)).forEach(System.out::println);
+        System.out.println();
+        System.out.println(Arrays.stream(fPrim(temp)).sum());
+        double licznik = Math.pow(Arrays.stream(fPrim(temp)).sum(), 2);
+        IntStream.range(0, temp.length).forEach(i -> temp2[i] = x[i] + ((krok+ Arrays.stream(temp).sum()) * g[i]));
+        double mianownik = Arrays.stream(fPrim(temp2)).sum() + Arrays.stream(fPrim(temp)).sum();
+        double nowyKrok = krok - licznik/mianownik;
+        System.out.println();
+        System.out.println("nowy krok: " + nowyKrok);
+//
+//        System.out.println("=======================================");
+//        for (int i1 = 0; i1 < temp.length; i1++) {
+//            temp[i1] = x[i1] + (nowyKrok * g[i1]);
+//        }
+//        System.out.println("\nnowe punkty bez optymalizacji kroku: ");
+//        Arrays.stream(temp).forEach(System.out::println);
+//        System.out.println("\nwartość funkcji z krokiem bez optymalizacji: " + Arrays.stream(f(temp)).sum());
+//        System.out.println();
+//        Arrays.stream(fPrim(temp)).forEach(System.out::println);
+//        System.out.println();
+//        System.out.println(Arrays.stream(fPrim(temp)).sum());
+//        licznik = Math.pow(Arrays.stream(fPrim(temp)).sum(), 2);
+//        for (int i = 0; i < temp.length; i++) {
+//            temp2[i] = x[i] + ((nowyKrok + Arrays.stream(temp).sum()) * g[i]);
+//        }
+//        mianownik = Arrays.stream(fPrim(temp2)).sum() + Arrays.stream(fPrim(temp)).sum();
+//        nowyKrok = krok - licznik/mianownik;
+//        System.out.println();
+//        System.out.println("nowy krok: " + nowyKrok);
+
+        System.out.println("ostateczna wartość iksów: ");
+        Arrays.setAll(x, i -> x[i] + nowyKrok * g[i]);
+        Arrays.stream(x).forEach(System.out::println);
+
+        System.out.print("\nwartość funkcji w tym punkcie: " + Arrays.stream(fPrim(x)).sum());
+
 
     }
 }
